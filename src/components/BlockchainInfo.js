b89cc333d5a6408307e09e3dd07e91730489175f
Cristiano Martins <cristiano.santos.martinss@gmail.com>
2018-10-14 21:33:18 +0100

    fixed positioning and set notification to not display on smaller screens

--- src/components/BlockchainInfo.js
+++ src/components/BlockchainInfo.js
@@ -108,6 +108,7 @@ class BlockchainInfo extends React.Component {
 
   setAssertsStatusState(state) {
     const { assertsNotification } = this.state;
+    console.log("state: ", state);
     if (state) {
       this.setState({
         assertsNotification: {
